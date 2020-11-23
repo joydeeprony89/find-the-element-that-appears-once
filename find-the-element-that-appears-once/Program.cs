a888982b17fa2ec8e9f98017ab10610838375211
Jaydeep Shil <ED8A@eurofins.com>
2020-11-23 18:23:00 +0530

    Add project files.

---
+++ find-the-element-that-appears-once/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace find_the_element_that_appears_once
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 100, 1, 34, 4, 1, 100, 34 };
+            Console.WriteLine(SingleNumber(nums));
+        }
+
+        static int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            foreach (int n in nums)
+                result ^= n;
+            return result;
+        }
+    }
+}
